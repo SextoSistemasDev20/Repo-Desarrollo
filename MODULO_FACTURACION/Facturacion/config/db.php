790454b5b7433e133088c339333015ac2f59bf1a
Bryan Lucio <bryandav91@gmail.com>
2020-02-05 01:02:01 -0500

    AGREGADO MODULO FACTURACION

---
+++ MODULO_FACTURACION/Facturacion/config/db.php
@@ -0,0 +1,21 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'root');//Usuario de tu base de datos
+define('DB_PASS', '');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'simple_invoice');//Nombre de la base de datos
+ 
+/*Datos de la empresa*/
+define('NOMBRE_EMPRESA', 'RECICLAR S.A.');
+define('DIRECCION_EMPRESA', 'Panamerican Sur km 3');
+define('TELEFONO_EMPRESA', '3433-552');
+define('EMAIL_EMPRESA', 'info@reciclar.com');
+define('TAX', '12');
+
+/*-------------------------------------------------
+#usuario: Faus
+#contraseña: noemi2403
+---------------------------------------------------*/
+
+
+?>
